d73b4823e2e75d45ff6f9f89db84b1f55568bd76
arniebradfo <arniebradfo@gmail.com>
2016-02-27 10:32:07 -0700

    deleted wp_title function

--- functions.php
+++ functions.php
@@ -68,29 +68,6 @@
 	}
 	add_action( 'after_setup_theme', 'html5reset_setup' );
 
-	// WP Title (based on twentythirteen: http://make.wordpress.org/core/tag/twentythirteen/)
-	function html5reset_wp_title( $title, $sep ) {
-		global $paged, $page;
-
-		if ( is_feed() )
-			return $title;
-
-//		 Add the site name.
-		$title .= get_bloginfo( 'name' );
-
-//		 Add the site description for the home/front page.
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) )
-			$title = "$title $sep $site_description";
-
-//		 Add a page number if necessary.
-		if ( $paged >= 2 || $page >= 2 )
-			$title = "$title $sep " . sprintf( __( 'Page %s', 'html5reset' ), max( $paged, $page ) );
-
-		return $title;
-	}
-	add_filter( 'wp_title', 'html5reset_wp_title', 10, 2 );
-
 	if ( !function_exists( 'load_theme_scripts_and_styles' ) ) {
 		function load_theme_scripts_and_styles() {
 			// Load Custom Styles
