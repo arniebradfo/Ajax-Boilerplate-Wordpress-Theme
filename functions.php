31068f074c8d42f750d0eb39cb01a01bab07390f
arniebradfo <arniebradfo@gmail.com>
2016-04-10 13:58:03 -0600

    added some more default options

--- functions.php
+++ functions.php
@@ -47,6 +47,7 @@
 
 	// Theme Setup (based on twentythirteen: http://make.wordpress.org/core/tag/twentythirteen/)
 	function wpajax_theme_setup() {
+
 		load_theme_textdomain( 'wpajax', get_template_directory() . '/languages' );
 
 		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
@@ -55,18 +56,42 @@
 		add_theme_support( 'post-formats', array( 'link', 'video', 'aside', 'audio', 'chat', 'gallery', 'image', 'quote', 'status' ) );
 		add_post_type_support( 'post', 'post-formats' );
 		add_post_type_support( 'page', 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
 
 		// Nav Menus
 		register_nav_menus( array(
 			'primary'   => __( 'Navigation Menu', 'wpajax' ),
 			'secondary' => __( 'Footer Menu', 'wpajax' ),
 		) );
+
+		// featured images aka thumbnails
+		add_theme_support( 'post-thumbnails' );
+ 		set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions: 150px width x 150px height (cropped)
+		update_option( 'thumbnail_size_w', 150 ); // don't depend on these 
+		update_option( 'thumbnail_size_h', 150 ); // the size of these can be edited by the user
+
+		// add image and manage image sizes
+		add_image_size( 'small', 100 );
+		update_option( 'medium_size_w', 300 ); // don't depend on these 
+		update_option( 'medium_size_h', 300 ); // the size of these can be edited by the user
+		update_option( 'medium_large_size_w', 768 );
+		update_option( 'medium_large_size_h', 768 );	
+		update_option( 'large_size_w', 1024 ); // don't depend on these 
+		update_option( 'large_size_h', 1024 ); // the size of these can be edited by the user
+		add_image_size( 'extralarge', 1600 );
+		// more image sizes is good for page speed now that srcset is in wp core:
+		// make.wordpress.org/core/2015/11/10/responsive-images-in-wordpress-4-4/
+
+		// change option defaults - https://codex.wordpress.org/Option_Reference
+		update_option('image_default_link_type', 'none');
+		update_option('image_default_align', 'none');
+		update_option('uploads_use_yearmonth_folders', 0); // keep all uploaded images in the same folder
+		update_option('use_smilies', 0); // becasue fuck smiling >:( 
+
 	}
 	add_action( 'after_setup_theme', 'wpajax_theme_setup' );
 
+	// Widgets
 	function wpajax_widget_setup() {
-		// Widgets
 		register_sidebar( array(
 			'name'          => __( 'Sidebar Widgets', 'wpajax' ),
 			'id'            => 'sidebar-primary',
@@ -78,7 +103,7 @@
 	}
 	add_action( 'widgets_init', 'wpajax_widget_setup' );
 
-	// ad custom css to style inside the tinyMCE editor
+	// add custom css to style inside the tinyMCE editor
 	function add_editor_styles() {
 		add_editor_style(); // path defaults to editor-style.css
 	}
